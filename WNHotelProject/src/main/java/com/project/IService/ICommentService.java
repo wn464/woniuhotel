535cd597e6d6e7645dc6308c2a978951991ee65a
zhouchengzhuang <97674657@qq.com>
2019-06-19 16:22:05 +0800

    修改包

--- WNHotelProject/src/main/java/com/project/IService/ICommentService.java
+++
@@ -1,9 +0,0 @@
-package com.project.IService;
-
-import com.project.bean.CommentBean;
-//评论业务层接口
-public interface ICommentService {
-	
-	public void addComment(CommentBean bean);
-
-}
--- WNHotelProject/src/main/java/com/project/IService/IPeopleService.java
+++
@@ -1,9 +0,0 @@
-package com.project.IService;
-
-import com.project.bean.PeopleBean;
-//入住人员业务接口
-public interface IPeopleService {
-	
-	public void addPeole(PeopleBean bean);
-
-}
