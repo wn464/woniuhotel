3e821cef576c203c89639c5267931072f5612247
zhangbo314159 <zhangbo_ss@126.com>
2019-06-19 19:37:31 +0800

    更改orderbean

--- WNHotelProject/src/main/java/com/project/bean/OrderBean.java
+++ WNHotelProject/src/main/java/com/project/bean/OrderBean.java
@@ -8,14 +8,29 @@ public class OrderBean {
 	private int id;//订单主键
 	private Timestamp orderTime;//订单生成时间
 	private String orderNumber;//订单号
-	private int status;//订单状态 失效，未支付、已支付
+	private MarkBean status;//订单状态 失效，未支付、已支付
 	private String alipayNumber;//支付宝号
 	private double payMoney;//押金
 	private double price;//订单金额
 	private MemberBean member;//订单所属用户
-	private int subcribeStatus;//订单是否处于预约状态
+	private MarkBean subcribeStatus;//订单是否处于预约状态
 	private CommentBean comment;//订单评论，避免sql死循环，不要直接查询，由comment进行维护
-	private List<LiveBean> lives;//居住房间数组
+	private LiveBean livaBean;//入住信息
+	
+	
+	
+	public MarkBean getStatus() {
+		return status;
+	}
+	public void setStatus(MarkBean status) {
+		this.status = status;
+	}
+	public MarkBean getSubcribeStatus() {
+		return subcribeStatus;
+	}
+	public void setSubcribeStatus(MarkBean subcribeStatus) {
+		this.subcribeStatus = subcribeStatus;
+	}
 	public int getId() {
 		return id;
 	}
@@ -34,12 +49,7 @@ public class OrderBean {
 	public void setOrderNumber(String orderNumber) {
 		this.orderNumber = orderNumber;
 	}
-	public int getStatus() {
-		return status;
-	}
-	public void setStatus(int status) {
-		this.status = status;
-	}
+	
 	public String getAlipayNumber() {
 		return alipayNumber;
 	}
@@ -64,30 +74,25 @@ public class OrderBean {
 	public void setMember(MemberBean member) {
 		this.member = member;
 	}
-	public int getSubcribeStatus() {
-		return subcribeStatus;
-	}
-	public void setSubcribeStatus(int subcribeStatus) {
-		this.subcribeStatus = subcribeStatus;
-	}
+	
 	public CommentBean getComment() {
 		return comment;
 	}
 	public void setComment(CommentBean comment) {
 		this.comment = comment;
 	}
-	public List<LiveBean> getLives() {
-		return lives;
+	public LiveBean getLivaBean() {
+		return livaBean;
 	}
-	public void setLives(List<LiveBean> lives) {
-		this.lives = lives;
+	public void setLivaBean(LiveBean livaBean) {
+		this.livaBean = livaBean;
 	}
 	@Override
 	public String toString() {
 		return "OrderBean [id=" + id + ", orderTime=" + orderTime + ", orderNumber=" + orderNumber + ", status="
 				+ status + ", alipayNumber=" + alipayNumber + ", payMoney=" + payMoney + ", price=" + price
-				+ ", member=" + member + ", subcribeStatus=" + subcribeStatus + ", comment=" + comment + ", lives="
-				+ lives + "]";
+				+ ", member=" + member + ", subcribeStatus=" + subcribeStatus + ", comment=" + comment + ", livaBean="
+				+ livaBean + "]";
 	}
 	
 	
