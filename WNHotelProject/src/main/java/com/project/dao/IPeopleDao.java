096973413f37f0d4fd5e3c4b32913ac49fd782c2
xiaolipk <1120091242@qq.com>
2019-06-19 16:32:55 +0800

    添加入住人员

--- WNHotelProject/src/main/java/com/project/dao/IPeopleDao.java
+++ WNHotelProject/src/main/java/com/project/dao/IPeopleDao.java
@@ -1,9 +1,12 @@
 package com.project.dao;
 
+import org.apache.ibatis.annotations.Insert;
+
 import com.project.bean.PeopleBean;
 //入住人员持久层接口
 public interface IPeopleDao {
-	
+	//添加入住信息
+	@Insert("insert into people(name,idCard,gender,liveid)values(#{name},#{idCard},#{gender.message},#{liveid})")
 	public void addPeole(PeopleBean bean);
 
 }
