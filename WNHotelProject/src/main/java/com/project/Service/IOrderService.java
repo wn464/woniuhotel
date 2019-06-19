26669b24f52efc18b378e3723ab0800a81d0c7e0
zhangbo314159 <zhangbo_ss@126.com>
2019-06-19 18:00:55 +0800

    修改dao层

--- WNHotelProject/src/main/java/com/project/Service/IOrderService.java
+++ WNHotelProject/src/main/java/com/project/Service/IOrderService.java
@@ -19,7 +19,7 @@ public interface IOrderService {
 	//前台和后台通过状态查询订单
 	public PageBean selectOrderByState(int mid,int state);
 	//后台通过开房人属性查询订单
-	public PageBean selectOrderByState(PeopleBean peopleBean);
+	public PageBean selectOrderByAttr(PeopleBean peopleBean);
 	//修改订单状态
 	public int updateOrderState(int oid,int state);
 	
--- WNHotelProject/src/main/java/com/project/dao/IPeopleDao.java
+++ WNHotelProject/src/main/java/com/project/dao/IPeopleDao.java
@@ -1,12 +1,23 @@
 package com.project.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Insert;
 
 import com.project.bean.PeopleBean;
-//入住人员持久层接口
+/**
+ * 入住人员持久层
+ * @author x1c
+ *
+ */
 public interface IPeopleDao {
-	//添加入住信息
-	@Insert("insert into people(name,idCard,gender,liveid)values(#{name},#{idCard},#{gender.message},#{liveid})")
-	public void addPeole(PeopleBean bean);
+	//添加入住人员信息
+	public int insertPeopleBean(PeopleBean peopleBean);
+	//删除入住人员信息
+	public int deletePeopleBean(int pid);
+	//模糊查询入住人员信息
+	public List<PeopleBean> selectPeopleBean(PeopleBean peopleBean);
+	//修改入住人员信息
+	public int upstatePeopleBean(int pid);
 
 }
