df45c563d75bc458301b410a7ecaaad2c96e61f9
zhaoxuechuan <2238421926@qq.com>
2019-06-19 15:28:57 +0800

    增加service包

---
+++ WNHotelProject/src/main/java/com/project/IService/IRService.java
@@ -0,0 +1,9 @@
+package com.project.IService;
+/***
+ * 房间业务层接口
+ * @author zxc
+ *
+ */
+public class IRService {
+
+}
---
+++ WNHotelProject/src/main/java/com/project/Serviceimpl/RoomService.java
@@ -0,0 +1,9 @@
+package com.project.Serviceimpl;
+/**
+ * 房间业务层实现
+ * @author lenovo
+ *
+ */
+public class RoomService {
+
+}
