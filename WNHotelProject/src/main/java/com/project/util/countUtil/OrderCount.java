4bfce982c8b929a547cc3b058062a5816dd366d0
LDnanchao <1393767576@qq.com>
2019-06-19 19:54:35 +0800

    订单计费，未加入计时，下一步完成计时统计

--- WNHotelProject/src/main/java/com/project/util/countUtil/OrderCount.java
+++ WNHotelProject/src/main/java/com/project/util/countUtil/OrderCount.java
@@ -4,7 +4,7 @@ package com.project.util.countUtil;
 import com.project.bean.DiscountBean;
 import com.project.bean.OrderBean;
 
-public class OrderCount {
+public class OrderCount extends DiscountCount{
 	/**
 	 * 检查优惠是否满足
 	 * @param order
@@ -19,5 +19,32 @@ public class OrderCount {
 			}
 		}
 		return true;
+	}
+	/**
+	 * 检查金额是否满足
+	 * @param order
+	 * @param discount
+	 * @return
+	 */
+	public boolean orderCheckMoney(OrderBean order,DiscountBean discount) {
+		if(discount.getNumber1()!=0) {
+			double price = getCount(order);
+			if (price<discount.getNumber1()) {
+				return false;
+			}
+		}
+		return true;
+	}
+	/**
+	 * 检查订单是否满足需求
+	 * @param order
+	 * @param discount
+	 * @return
+	 */
+	public boolean orderCheck(OrderBean order,DiscountBean discount) {
+		if(orderCheckMoney(order, discount) && orderCheckVip(order, discount)) {
+			return true;
+		}
+		return false;
 	}
 }
--- WNHotelProject/src/main/java/com/project/util/countUtil/OrderUtil.java
+++ WNHotelProject/src/main/java/com/project/util/countUtil/OrderUtil.java
@@ -6,7 +6,25 @@ import com.project.bean.OrderBean;
 
 public class OrderUtil extends OrderCount{
 
-	public double getFinalMoney(OrderBean order , DiscountBean discount) {
-		return 0;
+	public double getFinalMoney(OrderBean order , DiscountBean discount) throws Exception {
+		if(!orderCheck(order, discount)) {
+			throw new  Exception("订单优惠有误，请选择正确的优惠。");
+		}
+		double price = 0;
+		switch (discount.getDiscountType()) {
+		case 1:
+			price = getFullDiscountCount(order, discount);
+			break;
+		case 2:
+			price = getFixDiscountCount(order, discount);
+			break;
+		case 3:
+			price = getEveryMulitDiscountCount(order, discount);
+			break;
+		default:
+			price = getCount(order);
+			break;
+		}
+		return price;
 	}
 }
