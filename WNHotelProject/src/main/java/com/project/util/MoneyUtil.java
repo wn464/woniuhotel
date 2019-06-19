486fb6924a21f7898662145ed62148359be9a951
LDnanchao <1393767576@qq.com>
2019-06-19 18:16:15 +0800

    数值计算，大数字类型计算

---
+++ WNHotelProject/src/main/java/com/project/util/MoneyUtil.java
@@ -0,0 +1,62 @@
+package com.project.util;
+
+import java.math.BigDecimal;
+
+public class MoneyUtil {
+
+	private static int scale = 5;
+	private static BigDecimal start = new BigDecimal(1);
+	/**
+	 * 相除
+	 * @param number1
+	 * @param number2
+	 * @return
+	 */
+	public static double divide(double number1,double number2) {
+		BigDecimal num1 = new BigDecimal(number1);
+		BigDecimal num2 = new BigDecimal(number2);
+		BigDecimal res = num1.divide(num2, scale, BigDecimal.ROUND_HALF_UP);
+		return res.doubleValue();
+	}
+	/**
+	 * 相乘
+	 * @param number1
+	 * @param number2
+	 * @return
+	 */
+	public static double multiply(double number1,double number2) {
+		BigDecimal num1 = new BigDecimal(number1);
+		BigDecimal num2 = new BigDecimal(number2);
+		BigDecimal res = num1.multiply(num2);
+		return res.divide(start, scale, BigDecimal.ROUND_HALF_UP).doubleValue();
+	}
+	/**
+	 * 
+	 * 相加
+	 * @param number1
+	 * @param number2
+	 * @return
+	 */
+	public static double add(double number1,double number2) {
+		BigDecimal num1 = new BigDecimal(number1);
+		BigDecimal num2 = new BigDecimal(number2);
+		BigDecimal res = num1.add(num2);
+		return res.doubleValue();
+	}
+	/**
+	 * 相减
+	 * @param number1
+	 * @param number2
+	 * @return
+	 */
+	public static double subtract(double number1,double number2) {
+		BigDecimal num1 = new BigDecimal(number1);
+		BigDecimal num2 = new BigDecimal(number2);
+		BigDecimal res = num1.subtract(num2);
+		return res.doubleValue();
+	}
+	public static BigDecimal getBigDcimal(double number) {
+		BigDecimal num = new BigDecimal(number);
+		return num.divide(start, scale, BigDecimal.ROUND_HALF_UP);
+	}
+}
