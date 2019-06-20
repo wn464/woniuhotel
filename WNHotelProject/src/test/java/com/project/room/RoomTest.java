cbfc65f6660b4074bc8efc180dc789dcc01ecb83
zhaoxuechuan <2238421926@qq.com>
2019-06-20 17:42:13 +0800

    修改roommapper

--- WNHotelProject/src/test/java/com/project/room/RoomTest.java
+++ WNHotelProject/src/test/java/com/project/room/RoomTest.java
@@ -5,8 +5,11 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
 import com.project.Service.IRoomService;
-import com.project.bean.RoomBean;
+import com.project.bean.MarkBean;
+import com.project.bean.PageBean;
+import com.project.bean.TypeBean;
 import com.project.demo.WnHotelProjectApplication;
 
 
@@ -18,8 +21,12 @@ public class RoomTest {
        
 	    @Test
 	    public void insertTest(){
-	    	RoomBean room = ro.selectroombyid(1);
-	    	System.out.println(room);
+	    	TypeBean t=new TypeBean();
+	    	t.setId(1);
+	    	MarkBean m=new MarkBean();
+	    	m.setId(3);
+	    	PageBean page=ro.selectroombytypeandstatus(t, m,0, 1);
+	    	System.out.println(page);
 	    }
 	    @Test
 	    public void test1() {
