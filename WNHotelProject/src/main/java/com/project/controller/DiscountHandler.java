290e79b9a8aeaf3a36e886475f3d45603f165dae
LDnanchao <1393767576@qq.com>
2019-06-20 22:42:59 +0800

    优惠完善

--- WNHotelProject/src/main/java/com/project/controller/DiscountHandler.java
+++ WNHotelProject/src/main/java/com/project/controller/DiscountHandler.java
@@ -4,13 +4,21 @@ import java.util.List;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.Service.IDiscountService;
 import com.project.bean.DiscountBean;
 
 
+/**
+ * 优惠的handler
+ * @author my
+ *
+ */
 @RestController
 public class DiscountHandler {
 
@@ -20,7 +28,7 @@ public class DiscountHandler {
 	/**
 	 * 
 	 * @param price 费用
-	 * @param vipId 
+	 * @param vipId vip等级
 	 * @return
 	 */
 	public List<DiscountBean> selectDiscountByPriceAndMember(Double price,int vipId){
@@ -32,12 +40,39 @@ public class DiscountHandler {
 	 * 查询所有优惠
 	 * @return
 	 */
-	@GetMapping("/admin/AllDiscount")
+	@GetMapping("/admin/allDiscount")
 	public List<DiscountBean>  selectDiscountAll(){
 		List<DiscountBean> list = discountService.selectDiscountAll();
 		return list;
 	}
-	public int add(DiscountBean discount) {
-		return 0;
+	/**
+	 * 添加优惠
+	 * @param discount 优惠
+	 * @return 影响行数
+	 */
+	@PostMapping("/admin/discount")
+	public int insertDiscount(DiscountBean discount) {
+		int res = discountService.insertDiscount(discount);
+		return res;
+	}
+	/**
+	 * 修改优惠
+	 * @param discount 优惠
+	 * @return 影响行数
+	 */
+	@PutMapping("/admin/discount")
+	public int updateDiscount(DiscountBean discount) {
+		int res = discountService.updateDiscount(discount);
+		return res;
+	}
+	/**
+	 * 删除优惠
+	 * @param id 优惠的id
+	 * @return  影响行数
+	 */
+	@DeleteMapping("/admin/discount")
+	public int deleteDiscountById(int id) {
+		int res = discountService.deletDiscount(id);
+		return res;
 	}
 }
