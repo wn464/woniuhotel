60bf71a94364832f825d292bad6c50e3311ce20b
zhangxu9767 <97674647@qq.com>
2019-06-20 18:53:14 +0800

    liveDao完成

--- WNHotelProject/src/main/java/com/project/dao/ILiveDao.java
+++ WNHotelProject/src/main/java/com/project/dao/ILiveDao.java
@@ -16,11 +16,14 @@ public interface ILiveDao {
 	@Insert("insert into live (roomid,intime,outtime,orderid,phonenumber,people) values (#{roomid},#{inTime},#{outTime},#{orderid},#{phoneNumber},#{people})")
 	public int insertLiveBean(LiveBean liveBean);
 	
-	//通过时间段来查询这个时间段已经被预定过了的房间(ok)
+	//通过时间段来查询这个时间段已经被预定过了的房间(ok) 
 	public List<Integer> selectTime(String inTime,String outTime);
 	
 	//通过开房人名字和入住时间来查询入住信息  里面包含RoomBean(ok)
 	public LiveBean selectBypeopleAndDate(String people,String time);
+	
+	//通过订单号查询入住信息
+	public LiveBean findByorderid(String orderid);
 	
 	//删除入住信息
 	public int deleteLiveBean(int lid);
