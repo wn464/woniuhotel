6d4a4ea53a2945d5d6e7623bdbab37f2caf6af8a
xiaolipk <1120091242@qq.com>
2019-06-21 16:08:27 +0800

    添加评论方法

--- WNHotelProject/src/main/java/com/project/dao/ICommentDao.java
+++ WNHotelProject/src/main/java/com/project/dao/ICommentDao.java
@@ -1,5 +1,7 @@
 package com.project.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
 
@@ -8,9 +10,11 @@ import com.project.bean.OrderBean;
 //评论持久层接口
 public interface ICommentDao {
 	//添加评论
-	@Insert("insert into comment(orderId,message)values(#{orderId.orderNumber},#{message})")
+	@Insert("insert into comment(orderId,message,name,imgname)values(#{orderId.orderNumber},#{message},#{name},#{imgName})")
 	public void insert(CommentBean bean);
     //查询评论
-	@Select("select * from comment")
-	public CommentBean selectComment();
+	@Select("select * from comment limit #{page},#{size}")
+	public List<CommentBean> selectComment(int page,int size);
+	@Select("select count(*) from comment ")
+	public int selectAll();
 }
