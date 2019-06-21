1e5c2dbbeaaa9747dc91ffa6bcd1b1fb8fe56b29
xiaolipk <1120091242@qq.com>
2019-06-21 11:05:34 +0800

    .

--- WNHotelProject/src/main/java/com/project/controller/RoomHandler.java
+++ WNHotelProject/src/main/java/com/project/controller/RoomHandler.java
@@ -2,8 +2,12 @@ package com.project.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.project.Service.IRoomService;
 import com.project.bean.MarkBean;
@@ -11,13 +15,13 @@ import com.project.bean.PageBean;
 import com.project.bean.RoomBean;
 import com.project.bean.TypeBean;
 
-@Controller
+@RestController
 public class RoomHandler {
 	@Autowired
 	private IRoomService service;
-	@RequestMapping("/room")
-	@ResponseBody
-	public RoomBean selectroombyid(int rid) {
+	@GetMapping("/room{rid}")
+	public RoomBean selectroombyid(@PathVariable("rid")int rid) {
+	  System.out.println(rid);
       RoomBean bean = service.selectroombyid(rid);
       return bean;
 	}
