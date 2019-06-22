63e9cf8a65a0f889a2e4309e1cb59235d963f6f0
zhaoxuechuan <2238421926@qq.com>
2019-06-22 10:37:12 +0800

    完善定时器

--- WNHotelProject/src/main/java/com/project/util/timingutil/task.java
+++ WNHotelProject/src/main/java/com/project/util/timingutil/task.java
@@ -5,17 +5,37 @@ import java.util.TimerTask;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.project.Service.IOrderService;
+import com.project.bean.MarkBean;
+import com.project.bean.OrderBean;
 
 public class task extends TimerTask {
 	@Autowired
  private IOrderService service;
 	private int orderid;
-	public task(int ordernumber) {
+	
+	public task(IOrderService service,int ordernumber) {
 		this.orderid=ordernumber;
+		this.service=service;
 	}
 	@Override
 	public void run() {
-	
+		OrderBean oder=service.selectOrderById(orderid);
+		/**
+		 * 判断订单是否已支付
+		 */
+		if(oder.getStatus().getId()==6) {
+			
+		}else {
+			/**
+			 * 如果未支付修改订单为已超时
+			 */
+			OrderBean order=new OrderBean();
+			order.setId(orderid);
+			MarkBean status=new MarkBean();
+			status.setId(7);
+			order.setStatus(status);
+			service.updateOrderAttr(order);
+		}
 	}
 
 }
