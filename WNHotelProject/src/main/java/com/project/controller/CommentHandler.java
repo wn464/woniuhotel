6451da3df5e675915e56097719995021f321ff14
xiaolipk <1120091242@qq.com>
2019-06-24 11:25:04 +0800

    。

--- WNHotelProject/src/main/java/com/project/controller/CommentHandler.java
+++ WNHotelProject/src/main/java/com/project/controller/CommentHandler.java
@@ -1,5 +1,7 @@
 package com.project.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,6 +30,11 @@ public class CommentHandler {
 		System.out.println(i);
 		return i;
 	}
-	
+	@RequestMapping("/findComment")
+	@ResponseBody
+	public List<CommentBean> selectAll(){
+		List<CommentBean> list = service.selectAllComment();
+		return list;
+	}
 
 }
