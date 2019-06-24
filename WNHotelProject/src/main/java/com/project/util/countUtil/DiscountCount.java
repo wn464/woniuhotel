bd9fb223390c37b60e3a0d50788598d6da1afbe3
LDnanchao <1393767576@qq.com>
2019-06-24 17:50:47 +0800

    完善优惠

--- WNHotelProject/src/main/java/com/project/util/countUtil/DiscountCount.java
+++ WNHotelProject/src/main/java/com/project/util/countUtil/DiscountCount.java
@@ -2,20 +2,26 @@ package com.project.util.countUtil;
 
 import java.util.List;
 
+
+
+import com.project.Service.IDiscountService;
 import com.project.Service.IVipService;
 import com.project.bean.DiscountBean;
 import com.project.bean.LiveBean;
 import com.project.bean.OrderBean;
 import com.project.util.MoneyUtil;
-import com.project.util.TimeUtil;
 
 public class DiscountCount {
-
-	private IVipService vipService;
 	
-	public DiscountCount(IVipService vipService) {
+	protected IVipService vipService;
+	
+	protected IDiscountService discountService;
+	
+	
+	public DiscountCount(IVipService vipService, IDiscountService discountService) {
 		super();
 		this.vipService = vipService;
+		this.discountService = discountService;
 	}
 	/**
 	 * 初步统计金额
@@ -36,19 +42,23 @@ public class DiscountCount {
 		System.out.println("初步计算价格："+price);
 		return price;
 	}
+	protected List<DiscountBean> selectDiscounts(double price,int vipId){
+		List<DiscountBean> list = discountService.selectDiscountByVipIdAndPrice(vipId, price);
+		return list;
+	}
 	/**
 	 * 满减计算
 	 * @param order
 	 * @param discount
 	 * @return
 	 */
-	public double getFullDiscountCount(OrderBean order,DiscountBean discount) {
+	protected double getFullDiscountCount(OrderBean order,DiscountBean discount) {
 		//double price = getCount(order);
 		double price = order.getPrice();
 		price = MoneyUtil.subtract(price, discount.getNumber2());
 		return price;
 	}
-	public double getFullDiscountCount(double price,DiscountBean discount) {
+	protected double getFullDiscountCount(double price,DiscountBean discount) {
 		price = MoneyUtil.subtract(price, discount.getNumber2());
 		return price;
 	}
@@ -58,14 +68,14 @@ public class DiscountCount {
 	 * @param discount
 	 * @return
 	 */
-	public double getEveryMulitDiscountCount(OrderBean order,DiscountBean discount) {
+	protected double getEveryMulitDiscountCount(OrderBean order,DiscountBean discount) {
 		//double price = getCount(order);
 		double price = order.getPrice();
 		int i = (int)price/(int)discount.getNumber1();
 		price = MoneyUtil.subtract(price, discount.getNumber2()*i);
 		return price;
 	}
-	public double getEveryMulitDiscountCount(double price,DiscountBean discount) {
+	protected double getEveryMulitDiscountCount(double price,DiscountBean discount) {
 		int i = (int)price/(int)discount.getNumber1();
 		price = MoneyUtil.subtract(price, discount.getNumber2()*i);
 		return price;
@@ -73,13 +83,13 @@ public class DiscountCount {
 	/**
 	 * 满额折扣
 	 */
-	public double getFixDiscountCount(OrderBean order,DiscountBean discount) {
+	protected double getFixDiscountCount(OrderBean order,DiscountBean discount) {
 		//double price = getCount(order);
 		double price = order.getPrice();
 		price = MoneyUtil.multiply(price, discount.getNumber2());
 		return price;
 	}
-	public double getFixDiscountCount(double price,DiscountBean discount) {
+	protected double getFixDiscountCount(double price,DiscountBean discount) {
 		price = MoneyUtil.multiply(price, discount.getNumber2());
 		return price;
 	}
@@ -89,22 +99,23 @@ public class DiscountCount {
 	 * @param order
 	 * @return
 	 */
-	public double getVipDiscountCount(double price,OrderBean order) {
+	protected double getVipDiscountCount(double price,OrderBean order) {
 		double res = price;
 		//会员等级为0时，不计算
-		if(order.getMember().getVip()!=0) {
+		if(order.getMember()!=null && order.getMember().getVip()!=0) {
 			double discount = vipService.selectVipById(order.getMember().getVip()).getDiscount();
 			res = MoneyUtil.multiply(price, discount);
 		}
 		return res;
 	}
-	public double getVipDiscountCount(double price,int vipId) {
+	protected double getVipDiscountCount(double price,int vipId) {
 		double res = price;
 		//会员等级为0时，不计算
 		if(vipId!=0) {
 			double discount = vipService.selectVipById(vipId).getDiscount();
 			res = MoneyUtil.multiply(price, discount);
 		}
+		System.out.println("vip折扣后"+res);
 		return res;
 	}
 }
