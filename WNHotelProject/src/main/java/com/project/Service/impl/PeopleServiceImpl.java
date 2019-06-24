25231cf42912e4bc1994ffac0058e8d7f6cab44f
xiaolipk <1120091242@qq.com>
2019-06-24 09:48:45 +0800

    。

--- WNHotelProject/src/main/java/com/project/Service/impl/PeopleServiceImpl.java
+++ WNHotelProject/src/main/java/com/project/Service/impl/PeopleServiceImpl.java
@@ -6,17 +6,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.project.Service.IPeopleService;
+import com.project.bean.LiveBean;
+import com.project.bean.MarkBean;
+import com.project.bean.OrderBean;
 import com.project.bean.PeopleBean;
+import com.project.dao.IOrderDao;
 import com.project.dao.IPeopleDao;
 @Service
 public class PeopleServiceImpl implements IPeopleService{
 	@Autowired
 	private IPeopleDao dao;
+	@Autowired
+	private IOrderDao dao1;
 
 	@Override
-	public int insertPeopleBean(PeopleBean peopleBean) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int insertPeopleBean(String name,String idCard,int gende,int id) {
+		OrderBean orderBean = dao1.selectOrderById(id);
+		PeopleBean bean = new PeopleBean();
+		List<LiveBean> lives = orderBean.getLives();
+		int value =0;
+		for (LiveBean liveBean : lives) {
+			bean.setLiveId(liveBean.getId());
+			bean.setGende(gende);
+			bean.setIdCard(idCard);
+			bean.setName(name);
+			value += dao.insertPeopleBean(bean);
+		}
+		return value;
 	}
 
 	@Override
