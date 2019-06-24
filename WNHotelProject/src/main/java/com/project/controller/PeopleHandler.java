202082774d95a45db226044d8ff6c53b04b1fd95
xiaolipk <1120091242@qq.com>
2019-06-24 09:33:05 +0800

    添加入住信息

--- WNHotelProject/src/main/java/com/project/controller/PeopleHandler.java
+++ WNHotelProject/src/main/java/com/project/controller/PeopleHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.project.Service.IPeopleService;
+import com.project.bean.MarkBean;
 import com.project.bean.PeopleBean;
 
 @Controller
@@ -19,6 +20,12 @@ public class PeopleHandler {
 	public List<PeopleBean> selectPeopleBean(String name) {
 		List<PeopleBean> list = service.selectPeopleBean(name);
 		return list;
+	}
+	@RequestMapping("/addPeople")
+	@ResponseBody
+	public int insertPeopleBean(String name,String idCard,int gende,int id) {
+		int i = service.insertPeopleBean(name, idCard, gende, id);
+		return i;
 	}
 
 }
