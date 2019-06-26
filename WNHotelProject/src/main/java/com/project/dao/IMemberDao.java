5413a76b208a298421ac626fc9b7be65af8bae5d
zhouchengzhuang <97674657@qq.com>
2019-06-26 19:52:39 +0800

    54645

--- WNHotelProject/src/main/java/com/project/dao/IMemberDao.java
+++ WNHotelProject/src/main/java/com/project/dao/IMemberDao.java
@@ -1,6 +1,7 @@
 package com.project.dao;
 
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
