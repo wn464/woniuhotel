febd2627f56e57d513d0533a0d409e427cae2a13
zhouchengzhuang <97674657@qq.com>
2019-06-29 18:20:17 +0800

    asdsad

---
+++ WNHotelProject/src/main/java/com/project/filter/CustomRolesAuthorizationFilter.java
@@ -0,0 +1,31 @@
+package com.project.filter;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.web.filter.authz.AuthorizationFilter;
+
+
+public class CustomRolesAuthorizationFilter extends AuthorizationFilter{
+
+	@Override
+	protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue)
+			throws Exception {
+		
+		Subject subject = getSubject(request, response);  
+        String[] rolesArray = (String[]) mappedValue;  
+       
+        if (rolesArray == null || rolesArray.length == 0) { 
+            return false;  
+        }  
+        for (int i = 0; i < rolesArray.length; i++) {  
+           
+            if (subject.hasRole(rolesArray[i])) {
+                return true;  
+            }  
+        }     
+		return false;
+	}
+
+}
